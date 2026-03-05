edc2bc50584c0e95aa22690274d107ed9dc406e4
anmol <anmolsahota05760@gmail.com>
2026-03-05 21:50:57 +0530

    add a backend IP in env

--- src/ai-property/config.js
+++ src/ai-property/config.js
@@ -1,1 +1,3 @@
-export let BASE_URL="http://localhost:8081"
+// export let BASE_URL="http://localhost:8081"
+export const BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8081";
